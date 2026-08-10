9542950bbebd0249ef4b39be2650d83116a878e7
namankapoor7 <namankkapoor7@gmail.com>
2026-08-10 15:14:27 +0530

    Add CandidateRepository with jobId lookup and duplicate-check methods

---
+++ candidate-service/src/main/java/com/ijp/candidateservice/repository/CandidateRepository.java
@@ -0,0 +1,13 @@
+package com.ijp.candidateservice.repository;
+
+import com.ijp.candidateservice.entity.Candidate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+
+    List<Candidate> findByJobId(Long jobId);
+
+    boolean existsByEmailAndJobId(String email, Long jobId);
+}
