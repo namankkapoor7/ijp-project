1006b3b2ed96b8f1021d45176aa3b1b8c93cad4a
namankapoor7 <namankkapoor7@gmail.com>
2026-08-11 16:02:10 +0530

    Scaffold Angular frontend: routing, Bootstrap, service and component structure

---
+++ ijp-frontend/src/app/candidate-list/candidate-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-list',
+  imports: [],
+  templateUrl: './candidate-list.html',
+  styleUrl: './candidate-list.css',
+})
+export class CandidateList {}
